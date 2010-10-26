b2949aea239acc6e1012ae62acb77e58f54a27b0
James Parker <jimmparker@gmail.com>
2010-10-26 16:22:28 -0700

    more files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
   map.resources :playlists_songs
 
   map.resources :users_songs
@@ -8,6 +16,10 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :songs
 
   map.resources :users
+
+map.signup '/signup', :controller => 'users',   :action => 'new' 
+map.login  '/login',  :controller => 'session', :action => 'new'
+map.logout '/logout', :controller => 'session', :action => 'destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101015050533) do
+ActiveRecord::Schema.define(:version => 20101026231941) do
 
   create_table "playlists", :force => true do |t|
     t.string   "name"
@@ -39,13 +39,18 @@ ActiveRecord::Schema.define(:version => 20101015050533) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "pic"
-    t.string   "bio"
-    t.string   "email"
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
   end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
   create_table "users_songs", :id => false, :force => true do |t|
     t.integer  "user_id"
