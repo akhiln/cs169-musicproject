8799770ea0f0c33420b42371e4e6af9c63af389b
unknown <Brian@.(none)>
2010-11-02 13:52:13 -0700

    Fixed user creation step definition

--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -3,15 +3,16 @@ Given /^I am not signed in$/ do
   visit '/logout'
 end
 
-Given /^there is a profile named "(.*)" with password "(.*)$/ do |user_name, password|
+Given /^there is a profile named "([^"]*)"$/ do |user_name|
    @user = User.new  :name => user_name, :bio => "information!", :email => "asdfkj@asdfkkj.com",
-                :password => password, :login => user_name, :pic => "/images/default.jpg"
-   @user.save
+                :password => "password", :password_confirmation => "password",
+                :login => user_name, :pic => "/images/default.jpg"
+   @user.save!
 end
 
-Given /^I am signed in as "(.*) with password "(.*)"$/ do |user_name, password|
+Given /^I am signed in as "([^"]*)"$/ do |user_name|
    @user = User.find_by_name(user_name)
-   log_in_user!  :login => user_name, :password => password
+   log_in_user!  :login => user_name, :password => "password"
 end
 
 Given /^"(.)" has uploaded a song called "(.)"$/ do |user, song|
