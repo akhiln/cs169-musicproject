d38f69893fc417fbd7f941a5612787ecb57cc9fd
Akhil Naini <akhilnaini@gmail.com>
2010-11-01 18:44:02 -0700

    Removed scaffolding

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,50 +1,11 @@
-class SongsController < ApplicationController
-  # GET /songs
-  # GET /songs.xml
-  #before_filter :login_required, :only => [:edit, :create, :update, :destroy]
-  def index
-    @songs = Song.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @songs }
-    end
-  end
-
-  # GET /songs/1
-  # GET /songs/1.xml
-  def show
-    @song = Song.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @song }
-    end
-  end
-
-  # GET /songs/new
-  # GET /songs/new.xml
-  def new
-    @song = Song.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @song }
-    end
-  end
-
-  # GET /songs/1/edit
-  def edit
-    @song = Song.find(params[:id])
-  end
-
+class SongsController < ApplicationControllers
   # POST /songs
   # POST /songs.xml
   def create
     @song = Song.new(params[:song])
-	@song.upload(params[:upload])
     respond_to do |format|
       if @song.save
+		@song.upload(params[:upload])
         format.html { redirect_to(@song, :notice => 'Song was successfully created.') }
         format.xml  { render :xml => @song, :status => :created, :location => @song }
       else
@@ -54,31 +15,4 @@ class SongsController < ApplicationController
     end
   end
 
-  # PUT /songs/1
-  # PUT /songs/1.xml
-  def update
-    @song = Song.find(params[:id])
-
-    respond_to do |format|
-      if @song.update_attributes(params[:song])
-        format.html { redirect_to(@song, :notice => 'Song was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @song.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /songs/1
-  # DELETE /songs/1.xml
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(songs_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
