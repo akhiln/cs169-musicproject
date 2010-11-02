a0a2053ac90e4b82281f53a558465bca5352ca2a
Akhil Naini <akhilnaini@gmail.com>
2010-11-02 15:31:05 -0700

    Added rspec tests for models.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,19 +3,5 @@ class Playlist < ActiveRecord::Base
 	has_many :songs, :through => :playlistsongs
  	belongs_to :user
 	
-	validates_presence_of :name, :user_id
-	
-	def self.addSongToPlaylist(playlist_id, song_id)
-		plsong = PlaylistSongs.new
-		plsong.playlist_id = playlist_id
-		plsong.song_id = song_id
-		plsong.save
-	end
-	
-	def addSong(song_id)
-		plsong = Playlistsong.new
-		plsong.playlist_id = @id
-		plsong.song_id = song_id
-		plsong.save
-	end
+	validates_presence_of :name, :user_ids
 end
