e6885747e2aab2d9b95a1eecc01ee85015447523
James Parker <jimmparker@gmail.com>
2010-11-02 02:13:32 -0700

    added view_profiles story and fixed some user_controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,18 @@ class UsersController < ApplicationController
     success = @user && @user.save
     if success && @user.errors.empty?
       # save the profile picture:
-        @user.upload_picture(params[:upload])
+        if not params[:upload].empty?
+          @user.upload_picture(params[:upload])
+        else
+          @user.pic = "/images/default.jpg"
+          @user.save
+        end
       # Protects against session fixation attacks, causes request forgery
       # protection if visitor resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
       # reset session
       self.current_user = @user # !! now logged in
-      redirect_back_or_default('/')
+      redirect_to @user
       flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
