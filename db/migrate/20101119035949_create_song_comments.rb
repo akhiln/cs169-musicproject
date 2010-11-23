0938be3b4eac599df2e0dca627d01e195df32e55
James Parker <jimmparker@gmail.com>
2010-11-23 04:18:36 -0800

    hopefully fixed migrations

--- db/migrate/20101119035949_create_song_comments.rb
+++ db/migrate/20101119035949_create_song_comments.rb
@@ -2,8 +2,8 @@ class CreateSongComments < ActiveRecord::Migration
   def self.up
     create_table :song_comments do |t|
       t.string :text
-      t.int :user_id
-      t.int :song_id
+      t.integer :user_id
+      t.integer :song_id
 
       t.timestamps
     end
--- db/migrate/20101119040017_create_playlist_comments.rb
+++ db/migrate/20101119040017_create_playlist_comments.rb
@@ -2,8 +2,8 @@ class CreatePlaylistComments < ActiveRecord::Migration
   def self.up
     create_table :playlist_comments do |t|
       t.string :text
-      t.int :user_id
-      t.int :playlist_id
+      t.integer :user_id
+      t.integer :playlist_id
 
       t.timestamps
     end
