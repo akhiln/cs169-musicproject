c9b10fb41069788bf629ada732e55522f395f650
Brian Alker <bjalker@berkeley.edu>
2010-12-08 21:07:15 -0800

    Fixed cucumber to accept the new genre format

--- features/step_definitions/genre_steps.rb
+++ features/step_definitions/genre_steps.rb
@@ -1,5 +1,13 @@
 Given /^"([^"]*)" has created a song named "([^"]*)", with genre "([^"]*)"$/ do |user_name, song_name, song_genre|
   @user = User.find_by_name user_name
-  @user.songs.build({:name=>song_name, :genre=>song_genre})
-  @user.save
+  @genre = Genre.new({:genre_name=>song_genre})
+  @genre.save!
+  @song = @user.songs.build({:name=>song_name, :genre_id=>@genre.id})
+  @user.save!
+end
+
+
+Given /^there is a genre named "([^"]*)"$/ do |genre|
+  @genre = Genre.new({:genre_name=>genre})
+  @genre.save!
 end
