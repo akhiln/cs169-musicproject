0d355900f901ca743a2dd14ae483bcc282b4204b
Brian Alker <bjalker@berkeley.edu>
2010-12-13 19:36:40 -0800

    Genres are now ordered in the select boxes and browse

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -2,7 +2,7 @@ class BrowseController < ApplicationController
   #before_filter :require_user
 
   def all
-    @genres = Genre.find(:all, :include=>[:songs])
+    @genres = Genre.find(:all, :include=>[:songs], :order=>:genre_name)
     @genres.reject! do |genre|
       genre.songs.size == 0
     end
